0d18ed8ea465273c2eb2dbfa82b61917e2c29a29
Ethan Ardolino <ardolinoethan@gmail.com>
2023-11-26 21:12:26 -0500

    (ethan) reformatted Diary concept.

--- server/concepts/diary.ts
+++ server/concepts/diary.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
-import { NotFoundError } from "./errors";
+import { NotAllowedError, NotFoundError } from "./errors";
 
 export interface DiaryDoc extends BaseDoc {
   author: ObjectId;
@@ -22,14 +22,26 @@ export default class DiaryConcept {
     return { msg: "Created a diary entry.", diary: await this.diaries.readOne({ _id }) };
   }
 
+  async checkRep(user: ObjectId, _id: ObjectId) {
+    const diary = await this.diaries.readOne({ _id });
+    if (!diary) {
+      throw new NotFoundError("No diary associated with this id.");
+    }
+    if (user !== diary.author) {
+      throw new NotAllowedError("User is not the author of this diary.");
+    }
+  }
+
   /**
-   * Update the content of a diary entry
+   * Update a DiaryDoc within a collection
+   * @param user ObjectId associated with the current user
    * @param _id ObjectId associated with a specific diary entry
    * @param newContent updated content to associate w/ a diary entry
    * @returns updates 'this.diaries.content' to 'newContent'
    */
-  async modify(_id: ObjectId, newContent: string) {
-    await this.diaries.updateOne({ _id }, { content: newContent });
+  async updateDiary(user: ObjectId, _id: ObjectId, update: Partial<DiaryDoc>) {
+    await this.checkRep(user, _id);
+    await this.diaries.updateOne({ _id }, update);
     return { msg: "Updated diary entry." };
   }
 
@@ -38,38 +50,24 @@ export default class DiaryConcept {
    * @param _id ObjectId associated with a specific diary entry
    * @returns deletes diary w/ '_id' from 'this.diaries'
    */
-  async delete(_id: ObjectId) {
+  async delete(user: ObjectId, _id: ObjectId) {
+    await this.checkRep(user, _id);
     await this.diaries.deleteOne({ _id });
     return { msg: "Deleted diary entry." };
   }
 
   /**
-   * Find all diary entries written by a specific user
+   * Find all diary entries written by a specific user.
    * @param author ObjectId associated w/ the author
    * @returns all DiaryDocs in 'this.diaries' with 'this.diaries.author' === 'author'
+   *          If user !== author, then only show the visible diary entries.
    */
-  async getEntriesByAuthor(author: ObjectId) {
-    return await this.diaries.readMany({ author });
-  }
-
-  /**
-   * Reveal a diary entry to be seen by others
-   * @param _id ObjectId associated with a specific diary entry
-   * @returns sets 'this.diaries.revealed' = true
-   */
-  async revealEntry(_id: ObjectId) {
-    await this.diaries.updateOne({ _id }, { revealed: true });
-    return { msg: "Revealed diary entry." };
-  }
-
-  /**
-   * Hides a diary entry so it can not be seen by others
-   * @param _id ObjectId associated with a specific diary entry
-   * @returns sets 'this.diaries.revealed' = false
-   */
-  async hideEntry(_id: ObjectId) {
-    await this.diaries.updateOne({ _id }, { revealed: false });
-    return { msg: "Hid diary entry." };
+  async getEntriesByAuthor(user: ObjectId, author: ObjectId) {
+    let allEntries = await this.diaries.readMany({ author });
+    if (author !== user) {
+      allEntries = allEntries.filter((diary) => diary.revealed);
+    }
+    return allEntries;
   }
 
   /**
@@ -85,4 +83,38 @@ export default class DiaryConcept {
       return diary.revealed === false;
     }
   }
+  // /**
+  //  * Update the content of a diary entry
+  //  * @param user ObjectId associated with the current user
+  //  * @param _id ObjectId associated with a specific diary entry
+  //  * @param newContent updated content to associate w/ a diary entry
+  //  * @returns updates 'this.diaries.content' to 'newContent'
+  //  */
+  // async modify(user: ObjectId, _id: ObjectId, newContent: string) {
+  //   await this.isAuthor(user, _id);
+  //   await this.diaries.updateOne({ _id }, { content: newContent });
+  //   return { msg: "Updated diary entry." };
+  // }
+
+  // /**
+  //  * Reveal a diary entry to be seen by others
+  //  * @param _id ObjectId associated with a specific diary entry
+  //  * @returns sets 'this.diaries.revealed' = true
+  //  */
+  // async revealEntry(user: ObjectId, _id: ObjectId) {
+  //   await this.isAuthor(user, _id);
+  //   await this.diaries.updateOne({ _id }, { revealed: true });
+  //   return { msg: "Revealed diary entry." };
+  // }
+
+  // /**
+  //  * Hides a diary entry so it can not be seen by others
+  //  * @param _id ObjectId associated with a specific diary entry
+  //  * @returns sets 'this.diaries.revealed' = false
+  //  */
+  // async hideEntry(user: ObjectId, _id: ObjectId) {
+  //   await this.isAuthor(user, _id);
+  //   await this.diaries.updateOne({ _id }, { revealed: false });
+  //   return { msg: "Hid diary entry." };
+  // }
 }
