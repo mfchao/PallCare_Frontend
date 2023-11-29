c421a183597acee158c8a8e3d965cacc736ef1e8
vermouthwang <1184524959@qq.com>
2023-11-29 00:31:23 -0500

    implement and debugging email/ contact/ letter

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,6 @@
+import ContactConcept from "./concepts/contact";
 import DelayConcept from "./concepts/delay";
+import EmailConcept from "./concepts/email";
 import FriendConcept from "./concepts/friend";
 import LetterConcept from "./concepts/letter";
 import PostConcept from "./concepts/post";
@@ -16,3 +18,5 @@ export const Wish = new WishConcept();
 export const Topic = new TopicConcept();
 export const Delay = new DelayConcept();
 export const Letter = new LetterConcept();
+export const Email = new EmailConcept();
+export const Contact = new ContactConcept();
