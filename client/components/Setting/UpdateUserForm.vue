38b81f00e2df16d8cc05532732e662bf755a4cd4
mfchao <mfchao8@gmail.com>
2023-12-06 12:28:28 -0500

    settings styling

--- client/components/Setting/UpdateUserForm.vue
+++ client/components/Setting/UpdateUserForm.vue
@@ -21,12 +21,13 @@ async function updatePassword() {
 </script>
 
 <template>
+  <div class="section">
   <h2>Update user details</h2>
   <form @submit.prevent="updateUsername" class="pure-form">
     <fieldset>
       <legend>Change your username</legend>
       <input type="text" placeholder="New username" v-model="username" required />
-      <button type="submit" class="pure-button pure-button-primary">Update username</button>
+      <button type="submit" >Update username</button>
     </fieldset>
   </form>
 
@@ -34,7 +35,64 @@ async function updatePassword() {
     <fieldset>
       <legend>Change your password</legend>
       <input type="password" placeholder="New password" v-model="password" required />
-      <button type="submit" class="pure-button pure-button-primary">Update password</button>
+      <button type="submit" >Update password</button>
     </fieldset>
   </form>
+</div>
 </template>
+
+
+<style>
+h2 {
+  text-align: center;
+  letter-spacing: 0.06em;
+  font-size: 0.9em;
+  text-transform: uppercase;
+}
+
+.section {
+margin-top: 30px;
+}
+
+form {
+  display: flex;
+  gap: 0.5em;
+  align-items: center;
+  padding-left: 1em;
+  padding-top: 0.5em;
+}
+
+legend {
+  text-align: left;
+  font-size: 0.9em;
+}
+
+input {
+  font-size: 14px;
+  border-radius: 6px;
+  line-height: 1.5;
+  padding: 5px 10px;
+  transition: box-shadow 100ms ease-in, border 100ms ease-in, background-color 100ms ease-in;
+  color: rgb(14, 14, 16);
+  background:  rgba(255, 255, 255, 0.5);
+  display: block;
+  height: 36px;
+  -webkit-backdrop-filter: blur(8px);  /* Safari 9+ */
+  backdrop-filter: blur(8px); /* Chrome and Opera */
+  outline: none;
+  outline-width: 0;
+  border: none;
+  border-width: 0;
+  box-shadow: none;
+}
+
+button {
+  height: 40px;
+}
+
+
+button:hover {
+  background:rgba(255, 255, 255, 0.3); 
+}
+</style>
+
