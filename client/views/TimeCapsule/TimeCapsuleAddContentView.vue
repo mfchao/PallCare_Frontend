8507f99a3ae473d004d34e8acd2e2e1a27ad26e7
vermouthwang <1184524959@qq.com>
2023-12-07 21:37:35 -0500

    styling add time TimeCapsule

--- client/views/TimeCapsule/TimeCapsuleAddContentView.vue
+++ client/views/TimeCapsule/TimeCapsuleAddContentView.vue
@@ -31,8 +31,16 @@ onBeforeMount(async () => {
 </script>
 <template>
   <body>
-    <button @click="router.push({ name: 'TimeCapsuleContent' })">back</button>
-    <h1>Add Contents</h1>
+    <div class="navigation">
+      <img @click="router.push({ name: 'TimeCapsuleContent' })" src="@/assets/images/back.svg" />
+      <text class="pagetitle">Add Contents</text>
+    </div>
+    <div class="creatediv" @click="router.push({ path: `/letter/create/${true}` })">
+      <text class="createdivtitle">Write a new letter here</text>
+      <div class="blank">
+        <img src="@/assets/images/diaryicon.png" style="height: 200%; width: fit-content" />
+      </div>
+    </div>
     <button @click="router.push({ path: `/letter/create/${true}` })">Create a new letter to store in Time Capsule</button>
     <h1>Select Diaries to Add to Time Capsule</h1>
     <article v-for="diary in diaries" :key="diary._id">
@@ -49,4 +57,58 @@ onBeforeMount(async () => {
   </body>
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 60px 18px 120px 18px;
+  justify-content: space-between;
+  flex-direction: column;
+  background: #f0e7d8;
+  gap: 15px;
+}
+
+.creatediv {
+  display: flex;
+  width: 290px;
+  height: 89px;
+  padding-left: 14px;
+  align-items: center;
+  gap: 39px;
+  flex-shrink: 0;
+  border-radius: var(--numbers-spacing-12, 12px);
+  background: #9fb9c7;
+}
+.createdivtitle {
+  display: flex;
+  width: 162px;
+  height: 57px;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: #000;
+  font-family: SF Pro Display;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 103.822%;
+}
+.navigation {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.blank {
+  display: flex;
+  width: 68px;
+  height: 62px;
+  align-items: flex-end;
+  gap: 10px;
+  flex-shrink: 0;
+  border-radius: 7px;
+  background-color: #f0e7d8;
+}
+
+</style>
